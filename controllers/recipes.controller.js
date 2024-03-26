88ea77ccba12b5ffadd97d5c9bd68ee5d379d77b
ednaakpene <edvalerie18@gmail.com>
2024-03-26 09:25:06 +0000

    added try catch to the controllers

--- controllers/recipes.controller.js
+++ controllers/recipes.controller.js
@@ -1,28 +1,45 @@
 import { RecipeModel } from "../models/recipe.js";
 
+export const addRecipe = async (req, res) => {
+  // add recipe to databse
+  const createResult = await RecipeModel.create(req.body);
+  // return response
+  res.status(201).json(createResult);
+};
 
-export const addRecipe = async (req,res) => {
-    // add recipe to databse
-const createResult = await RecipeModel.create(req.body);
-    // return response
-    res.status(201).json(createResult);
-    }
-
-export const getRecipes = async (req,res) =>{
+export const getRecipes = async (req, res, next) => {
+  try {
     // get all recipes from database
-    const findResult = await RecipeModel.find();
+    const results = await RecipeModel.find();
     // return response
-    res.status(200).json(findResult);
-}
+    res.status(200).json(results);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getRecipe = (req,res) => {
-    res.send("get single recipe");
-}
+export const getRecipe = async (req, res, next) => {
+  try {
+    // get single recipe by id
+    const findByIdResult = await RecipeModel.findById(req.params.id);
+    // return 404 if no recipe is found
+    if (findByIdResult === null) {
+      return res.status(404).json({
+        message: `Recipe with ID: ${req.params.id} not found`,
+      });
+    } else {
+      // return response
+      res.status(200).json(findByIdResult);
+    }
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const updateRecipe = (req,res) => {
-    res.send(`update single recipe with id: ${req.params.id}`)
-}
+export const updateRecipe = (req, res) => {
+  res.send(`update single recipe with id: ${req.params.id}`);
+};
 
-export const deleteRecipe = (req,res) =>{
-    res.send("delete single recipe");
-}
+export const deleteRecipe = (req, res) => {
+  res.send("delete single recipe");
+};
